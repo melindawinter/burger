d049f356b7a453a6273bc8fef45161600c429c78
Melinda Winter <melindawinter42@gmail.com>
2020-08-07 16:51:04 -0700

    build database

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portobella Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Beet Burger');
+INSERT INTO burgers (burger_name) VALUES ('Lentil Burger');
