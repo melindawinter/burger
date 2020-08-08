09721bc12f902781c1c68a2eb0780ea8fb32d6c2
Melinda Winter <melindawinter42@gmail.com>
2020-08-08 16:25:55 -0700

    tring to fix js errors

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,5 @@
-// TODO - this may be ("../config/connection.js") Import connection
-var connection = require("./connection.js");
+//Import connection
+var connection = require("../config/connection.js");
 
 // Helper function for SQL syntax.
 
@@ -82,20 +82,7 @@ var orm = {
       cb(result);
     });
   },
-  deleteOne: function (table, condition, cb) {
-    var queryString = "DELETE FROM " + table;
-    queryString += " WHERE ";
-    queryString += condition;
-
-    connection.query(queryString, function (err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
 };
 
-// Export the orm object for the model (cat.js).
+// Export the orm object for the model (burger.js).
 module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -36,9 +36,9 @@ router.put("/api/burgers/:id", function (req, res) {
 
   console.log("condition", condition);
 
-  burger.update(
+  burger.updateOne(
     {
-      sleepy: req.body.sleepy,
+      devoured: req.body.devoured,
     },
     condition,
     function (result) {
@@ -50,19 +50,6 @@ router.put("/api/burgers/:id", function (req, res) {
       }
     }
   );
-});
-
-router.delete("/api/burgers/:id", function (req, res) {
-  var condition = "id = " + req.params.id;
-
-  burger.deleteOne(condition, function (result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
 });
 
 // Export routes for server.js to use.
